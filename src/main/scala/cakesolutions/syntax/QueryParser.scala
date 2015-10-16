2b708108a291b91e4137781f723dbbe6a1ec4611
Carl Pulley <carlp@cakesolutions.net>
2015-10-16 08:40:19 +0100

    Fixed amibiguous negation in grammar

--- src/main/scala/cakesolutions/syntax/QueryParser.scala
+++ src/main/scala/cakesolutions/syntax/QueryParser.scala
@@ -7,16 +7,19 @@ object QueryParser extends RegexParsers {
   lazy val GroundFact: Parser[QueryLanguage.GroundFact] =
     "[_a-zA-Z][_a-zA-Z0-9]*".r.filter(f => !QueryLanguage.keywords.contains(f)) ^^ QueryLanguage.GroundFact
 
-  // FIXME: enable unambiguous use of not!
   lazy val Proposition: Parser[QueryLanguage.Proposition] =
     "true" ^^ (_ => QueryLanguage.True) |
+      "~" ~> "true" ^^ (_ => QueryLanguage.False) |
       "false" ^^ (_ => QueryLanguage.False) |
+      "~" ~> "false" ^^ (_ => QueryLanguage.True) |
       GroundFact ^^ QueryLanguage.Assert |
       "~" ~> GroundFact ^^ (QueryLanguage.Neg andThen QueryLanguage.Assert) |
-      //"~" ~> Proposition ^^ QueryLanguage.not |
       Proposition ~ ("&&" ~> Proposition).+ ^^ { case (p, ps) => QueryLanguage.Conjunction(p, ps.head, ps.tail: _*) } |
+      "~" ~> Proposition ~ ("&&" ~> Proposition).+ ^^ { case (p, ps) => QueryLanguage.Conjunction(QueryLanguage.not(p), ps.head, ps.tail: _*) } |
       Proposition ~ ("||" ~> Proposition).+ ^^ { case (p, ps) => QueryLanguage.Disjunction(p, ps.head, ps.tail: _*) } |
-      "(" ~> Proposition <~ ")" ^^ identity
+      "~" ~> Proposition ~ ("||" ~> Proposition).+ ^^ { case (p, ps) => QueryLanguage.Disjunction(QueryLanguage.not(p), ps.head, ps.tail: _*) } |
+      "(" ~> Proposition <~ ")" ^^ identity |
+      "~" ~> "(" ~> Proposition <~ ")" ^^ QueryLanguage.not
 
   // FIXME: deal with ambiguity!
   lazy val Path: Parser[QueryLanguage.Path] =
@@ -28,16 +31,22 @@ object QueryParser extends RegexParsers {
       Proposition ^^ QueryLanguage.AssertFact
 
   // FIXME: deal with ambiguity!
-  // FIXME: enable unambiguous use of not!
   lazy val Query: Parser[QueryLanguage.Query] =
     "TT" ^^ (_ => QueryLanguage.TT) |
+      "~" ~> "TT" ^^ (_ => QueryLanguage.FF) |
       "FF" ^^ (_ => QueryLanguage.FF) |
-      //"~" ~> Query ^^ QueryLanguage.not |
+      "~" ~> "FF" ^^ (_ => QueryLanguage.TT) |
       "<" ~> Path ~ ">" ~ Query ^^ { case (p, _, q) => QueryLanguage.Exists(p, q) } |
+      "~" ~> "<" ~> Path ~ ">" ~ Query ^^ { case (p, _, q) => QueryLanguage.All(p, QueryLanguage.not(q)) } |
       "[" ~> Path ~ "]" ~ Query ^^ { case (p, _, q) => QueryLanguage.All(p, q) } |
+      "~" ~> "[" ~> Path ~ "]" ~ Query ^^ { case (p, _, q) => QueryLanguage.Exists(p, QueryLanguage.not(q)) } |
       Query ~ ("&&" ~> Query).+ ^^ { case (q, qs) => QueryLanguage.And(q, qs.head, qs.tail: _*) } |
+      "~" ~> Query ~ ("&&" ~> Query).+ ^^ { case (q, qs) => QueryLanguage.And(QueryLanguage.not(q), qs.head, qs.tail: _*) } |
       Query ~ ("||" ~> Query).+ ^^ { case (q, qs) => QueryLanguage.Or(q, qs.head, qs.tail: _*) } |
+      "~" ~> Query ~ ("||" ~> Query).+ ^^ { case (q, qs) => QueryLanguage.Or(QueryLanguage.not(q), qs.head, qs.tail: _*) } |
       "(" ~> Query <~ ")" ^^ identity |
-      Proposition ^^ QueryLanguage.Formula
+      "~" ~> "(" ~> Query <~ ")" ^^ QueryLanguage.not |
+      Proposition ^^ QueryLanguage.Formula |
+      "~" ~> Proposition ^^ (f => QueryLanguage.Formula(QueryLanguage.not(f)))
 
 }
